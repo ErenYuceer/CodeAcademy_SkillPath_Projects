8bf3c91b0fb87f9cfd6a554c12b71423b411845d
ErenYuceer <65268957+ErenYuceer@users.noreply.github.com>
2020-05-20 01:29:41 -0400

    Create function_challanges.py

---
+++ function_challanges.py
@@ -0,0 +1,8 @@
+# Write a function named tenth_power() that has one parameter named num.
+# The function should return num raised to the 10th power.
+def tenth_power(n):
+  return n**10
+
+print(tenth_power(1))
+print(tenth_power(0))
+print(tenth_power(2))
