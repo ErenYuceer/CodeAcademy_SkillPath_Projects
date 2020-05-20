438de43816f0c0919299c4a85ec7b036259726e4
ErenYuceer <65268957+ErenYuceer@users.noreply.github.com>
2020-05-20 03:05:24 -0400

    Update function_challanges.py

--- function_challanges.py
+++ function_challanges.py
@@ -52,3 +52,56 @@ print(remainder(15, 14))
 # should print 2
 print(remainder(9, 6))
 # should print 0
+
+#Write a function named first_three_multiples() that has one parameter named num.
+#This function should print the first three multiples of num. Then, it should return the third multiple.
+#For example, first_three_multiples(7) should print 7, 14, and 21 on three different lines, and return 21.
+
+def first_three_multiples(num):
+  print(num)
+  print(num*2)
+  print(num*3)
+  return(num*3)
+
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
+
+#Create a function called tip() that has two parameters named total and percentage.
+#This function should return the amount you should tip given a total and the percentage you want to tip.
+
+def tip(total,percentage):
+  return total*percentage*0.01
+
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
+
+#Write a function named introduction() that has two parameters named first_name and last_name.
+#The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
+
+def introduction(first_name,last_name):
+  return last_name +", " + first_name +" " +  last_name
+
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
+
+#Some say that every one year of a human’s life is equivalent to seven years of a dog’s life. Write a function named dog_years() that has two parameters named name and age.
+#The function should compute the age in dog years and return the following string:
+#"{name}, you are {age} years old in dog years"
+# Test this function with your name and your age!
+
+def dog_years(name,age):
+  dog_age= age*7
+  return name + ", you are "+str(dog_age) + " years old in dog years"
+
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+print(dog_years("Eren", 27))
+
