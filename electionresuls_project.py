e15a046589c7b0a67f4a1079732a4bea56cceaf2
ErenYuceer <65268957+ErenYuceer@users.noreply.github.com>
2020-07-18 21:01:13 -0400

    Update electionresuls_project.py

--- electionresuls_project.py
+++ electionresuls_project.py
@@ -8,20 +8,21 @@ survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','
 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
 
-total_ceballos = survey_responses.count('Ceballos')
-print(total_ceballos)
+total_ceballos = survey_responses.count( 'Ceballos' )
+print( total_ceballos )
 
-survey_length = len(survey_responses)
-percentage_ceballos = total_ceballos/survey_length
-print(percentage_ceballos)
+survey_length = len( survey_responses )
+percentage_ceballos = total_ceballos / survey_length
+print( percentage_ceballos )
 
-possible_surveys = np.random.binomial(survey_length, .54, size=10000) / survey_length
-plt.hist(possible_surveys, range =(0 , 1), bins=20)
+possible_surveys = np.random.binomial( survey_length, .54, size=10000 ) / survey_length
+plt.hist( possible_surveys, range=(0, 1), bins=20 )
 plt.show()
-ceballos_loss_surveys = np.mean(possible_surveys < 0.5)
-print(ceballos_loss_surveys)
 
-large_survey = np.random.binomial( survey_length, .54, size=7000 ) / survey_length
+ceballos_loss_surveys = np.mean( possible_surveys < 0.5 )
+print( ceballos_loss_surveys )
+
+large_survey = np.random.binomial( 7000, .54, size=10000 ) / 7000
 
 ceballos_loss_new = np.mean(large_survey < 0.5)
 print(ceballos_loss_new)
